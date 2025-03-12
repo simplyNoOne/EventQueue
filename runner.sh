476515dacc5c92479f52f0e16cdeadaae7d02976
Anna Sidor <annasidor00@gmail.com>
2025-03-12 22:04:16 +0100

    config script

--- runner.sh
+++ runner.sh
@@ -4,16 +4,39 @@
 cleanup() {
     echo "Stopping all running Java processes..."
     # Kill all background processes (jobs)
-    kill $pid1
+    kill $p1_1 $p1_2 $p1_3 $p2_1 $p3_1 $c1_1 $c1_2 $c2_1 $c3_1 $c4_1
     exit 0
 }
 
 # Trap SIGINT (Ctrl+C) to call the cleanup function
 trap cleanup SIGINT
 
-# Run the JAR file three times in the background
-java -jar Type1Publisher.jar & pid1=$!
+# Publisher 1
+java -jar artifacts/Type1Publisher.jar & p1_1=$!
+java -jar artifacts/Type1Publisher.jar & p1_2=$!
+java -jar artifacts/Type1Publisher.jar & p1_3=$!
+
+# Publisher 2
+java -jar artifacts/Type2Publisher.jar & p2_1=$!
+
+# Publisher 3
+java -jar artifacts/Type3Publisher.jar & p3_1=$!
+
+# Consumer 1
+java -jar artifacts/Type1Consumer.jar & c1_1=$!
+java -jar artifacts/Type1Consumer.jar & c1_2=$!
+
+# Consumer 2
+java -jar artifacts/Type2Consumer.jar & c2_1=$!
+
+# Consumer 3
+java -jar artifacts/Type3Consumer.jar & c3_1=$!
+
+# Consumer 4
+java -jar artifacts/Type4Consumer.jar & c4_1=$!
+
 
 # Wait for any of the background jobs to finish or be cancelled
-wait $pid1
+wait $p1_1
+wait $c4_1
 
